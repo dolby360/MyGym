15bbbdcc97e65d85341dfeacc1641fd784b1cf65
dolev.ben-aharon <dolev.ben-aharon@hp.com>
2019-10-21 12:44:39 +0300

    add header

---
+++ components/header.js
@@ -0,0 +1,10 @@
+import React , {Component} from 'react';
+import {Image} from 'react-native';
+
+export default Header = () => {
+    return(
+        <View>
+            
+        </View>
+    );
+} 
